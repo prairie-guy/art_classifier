008231801e7ed4bab4c70d947a30643a960856c0
prairie-guy <cdaniels@nandor.net>
2019-06-16 20:01:50 -0500

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/ky36nbjjjphfzor/export.pk1?dl=1' # soccer-player or football-player
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+#classes = ['black', 'grizzly', 'teddys']
+#classes = ['soccer-player', 'football-player']
 path = Path(__file__).parent
 
 app = Starlette()
